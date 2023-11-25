6bfec4126f8159e5f0ba4e7e97d230d7ec1da811
gamze <gamzekandara@gmail.com>
2023-11-25 15:55:01 -0500

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer
+const leastFavoriteCustomer = 'gamze'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName
+}
+//ASK: why tests fail when I invoke upperCaseCustomerName function here
+// upperCaseCustomerName()
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'ogulcan'
+}
